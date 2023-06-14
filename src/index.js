3e30b818f05a3c8cf6e9b34480cf334e8b8ac48f
Ranjeet <ranjeet0001@gmail.com>
2023-06-14 20:10:24 +0530

    XOLO

--- src/index.js
+++ src/index.js
@@ -66,7 +66,7 @@ function Header() {
   const style = {};
   return (
     <div className="header">
-      <h1 style={style}>Fast React Pizza Co.</h1>;
+      <h1 style={style}>Fast React Pizza Co.</h1>
     </div>
   );
 }
